2f231d44f5d0e9839361ef224f4fa807e0b78bcf
Ian Evit <Administrator@MININT-EEJ88UO>
2022-11-08 12:18:15 -0700

    Add timer Stopwatch

--- CSharpAlgorithms/Program.cs
+++ CSharpAlgorithms/Program.cs
@@ -5,8 +5,19 @@ using CSharpAlgorithms.TestingHelpers;
 namespace CSharpAlgorithms {
     class Program {
         static void Main(string[] args) {
-            int[] result = MergeSort.Sort(IntPopulator.Random(10, unique: true));
+            StopWatch stopwatch = new StopWatch();
+            int[] population = IntPopulator.Random(10, unique: true);
+            Console.WriteLine(stopwatch.GetTime() + " milliseconds to create population.");
 
+            stopwatch.ResetTime();
+            int[] result = MergeSort.Sort(population);
+            Console.WriteLine(stopwatch.GetTime() + " milliseconds to sort population.");
+
+            Console.Write("\nRandom: ");
+            foreach (var num in population) {
+                Console.Write(num + " ");
+            }
+            Console.Write("\nSorted: ");
             foreach (var num in result) {
                 Console.Write(num + " ");
             }
---
+++ CSharpAlgorithms/TestingHelpers/Timers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAlgorithms.TestingHelpers {
+    class StopWatch {
+        private DateTime startTime;
+
+        public StopWatch() {
+            ResetTime();
+        }
+
+        public double GetTime() {
+            return (DateTime.Now - startTime).TotalMilliseconds;
+        }
+
+        public void ResetTime() {
+            startTime = DateTime.Now;
+        }
+    }
+}
